00a78f20f110c30f92308374260edc5c96e9338f
Y <35826375+BeGentleman@users.noreply.github.com>
2020-09-13 22:32:01 +0900

    汉诺塔python实现

---
+++ hannuota.py
@@ -0,0 +1,14 @@
+#Author:Y
+#2020.09.13
+
+def move(n,a,b,c):
+    
+    if n==1:
+        print(a,'---->',c)
+    else:
+        move(n-1,a,c,b)
+        print(a,'---->',c)
+        move(n-1,b,a,c)
+        
+n = int(input("请输入n的值："))
+move(n,"A","B","C")
